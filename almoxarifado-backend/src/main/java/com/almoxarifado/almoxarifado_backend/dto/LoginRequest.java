8508d673b05c8262a91d4e12cd0b8ca71b1f8db2
Fernandes-Bernardo <bernardofpdev@gmail.com>
2025-10-22 00:55:50 -0300

    feat(dto): add JWT authentication DTOs (LoginRequest, AuthResponse, and LoginResponse)

---
+++ almoxarifado-backend/src/main/java/com/almoxarifado/almoxarifado_backend/dto/LoginRequest.java
@@ -0,0 +1,21 @@
+package com.almoxarifado.almoxarifado_backend.dto;
+
+public class LoginRequest {
+    private String username;
+    private String password;
+
+
+    // getters e setters
+        public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
