4685ddd55aec22bbf86d6deee15bca7f5bdd4c2e
Fernandes-Bernardo <bernardofpdev@gmail.com>
2025-10-26 23:31:08 -0300

    feat(app): add routes to the pages

--- almoxarifado-frontend/src/App.jsx
+++ almoxarifado-frontend/src/App.jsx
@@ -1,5 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Produtos from './pages/Produtos';
+import Entradas from './pages/Entradas';
+import Saidas from './pages/Saidas';
+import Movimentacoes from './pages/Movimentacoes';
+import Emprestimos from './pages/Emprestimos';
+import Logs from './pages/Logs';
 import Login from './pages/Login';
 
 export default function App() {
@@ -8,6 +13,11 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/produtos" element={<Produtos />} />
+        <Route path="/entradas" element={<Entradas />} />
+        <Route path="/saidas" element={<Saidas />} />
+        <Route path="/movimentacoes" element={<Movimentacoes />} />
+        <Route path="/emprestimos" element={<Emprestimos />} />
+        <Route path="/logs" element={<Logs />} />
       </Routes>
     </Router>
   );
